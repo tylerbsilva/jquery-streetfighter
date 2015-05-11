bc4706f1dc14456bba72b95c2cc756035512d0c5
Tyler Silva <tylerbsilva@gmail.com>
2015-05-11 13:07:37 -0500

    Added basic jQuery to change betweens Ryos

---
+++ js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	$('.ryu').mouseenter(function(){
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function(){
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
+		//play hadouken
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+			.animate(
+			  	{'left': '1020px'},
+	 		  	500,
+	  		  	function() {
+	    			$(this).hide();
+	    			$(this).css('left', '520px');
+  				}
+			);
+		//show hadouken and animate it to the right
+	})
+	.mouseup(function(){
+		//ryu reverts to ready
+		$('.ryu-throwing').hide();
+		$('.hadouken').hide();
+		$('.ryu-ready').show();
+	});
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
